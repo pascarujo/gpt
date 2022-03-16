518eeac202c03661d466ce87f9e7904709412e73
pascarujo <pascarujo@gmail.com>
2022-03-16 14:14:02 -0300

    Update settings.py

--- gpt_site/settings.py
+++ gpt_site/settings.py
@@ -85,7 +85,8 @@ DATABASES = {
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST')
+        'HOST': config('DB_HOST'),
+        'PORT':config('DB_PORT', cast=int)
     }
 }
 
